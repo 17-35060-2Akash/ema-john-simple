fdd016902bb35a64d5c6774070b87c1f6e844aae
Akash <na.akash715@gmail.com>
2022-09-26 17:38:07 +0600

    done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart }) => {
+    // console.log(cart);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        // console.log(quantity);
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+    }
+
+    const tax = (parseFloat(totalPrice * 0.1));
+    const grandTotal = (totalPrice + totalShipping + tax).toFixed(2);
     return (
-        <div>
-            <h4>Order summary</h4>
-            <p>Selected Items: {cart.length}</p>
+        <div className='cart'>
+            <h4 className='summary'>Order Summary</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h4>Grand Total: ${grandTotal}</h4>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -15,10 +16,37 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
-    const handleAddToCart = (product) => {
-        console.log(product);
-        const newCart = [...cart, product];
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        let savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+                // console.log(savedCart);
+                setCart(savedCart);
+            }
+        }
+    }, [products]);
+
+    const handleAddToCart = (selectedProduct) => {
+        console.log(selectedProduct);
+        let newCart = [];
+        const existes = cart.find(product => product.id === selectedProduct.id);
+        if (!existes) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id != selectedProduct.id);
+            existes.quantity = existes.quantity + 1;
+            newCart = [...rest, existes];
+        }
+
         setCart(newCart);
+        addToDb(selectedProduct.id);
     };
 
     return (
